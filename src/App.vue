13de6a40fe42a2d81995e53270642a25168ca3e0
Noguchi-maron <mimikkku1radi@gmail.com>
2021-09-13 03:47:13 +0900

    作成からカスタマイズまで

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click="toggleSideMenu" v-show="$store.state.login_user"></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        <v-app-bar-title>
+          <span>マイアドレス帳</span>
+        </v-app-bar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn @click="logout" v-if="$store.state.login_user">ログアウト</v-btn>
+    </v-app-bar>
+    <SideNav/>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import firebase from 'firebase'
+import { mapActions } from 'vuex'
+import SideNav from './components/SideNav.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setLoginUser(user)
+        this.fetchAddresses()
+        if (this.$router.currentRoute.name === 'home') this.$router.push({ name: 'addresses' })
+      } else {
+        this.deleteLoginUser()
+        this.$router.push({ name: 'home' })
+      }
+    })
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    
+    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser', 'fetchAddresses'])
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import firebase from 'firebase'
+
 
 Vue.config.productionTip = false
+
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCMejH3CZspZ88FEuWDl1_BPxfVo7KDtgU",
+    authDomain: "my-address-pj-9ffeb.firebaseapp.com",
+    projectId: "my-address-pj-9ffeb",
+    storageBucket: "my-address-pj-9ffeb.appspot.com",
+    messagingSenderId: "230378020966",
+    appId: "1:230378020966:web:8e2d0deb26e7348da1cda7",
+    measurementId: "G-JRSFDRC8ML"
+  };
+
+  firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
   store,
+  vuetify,
+  firebase,
   render: h => h(App)
 }).$mount('#app')
